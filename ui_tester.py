2ad238ac19e4098eb5107a0a393200e1ba5c84f6
Daniel Zimmermann <dzimmer@fzi.de>
2023-06-23 09:40:32 +0200

    Added print statements

--- ui_tester.py
+++ ui_tester.py
@@ -1,6 +1,9 @@
 import click
+import os.path
 import time
 import openai
+from pathlib import Path
+import urllib.parse
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -38,6 +41,16 @@ import string
     help="The type of testing to perform.",
 )
 def main(url, delay, interactions, load_wait_time, test_type):
+    # Check if the given URL is a local file path
+    if os.path.isfile(url):
+        # If it is a local file, convert the file path to a proper URL
+        url = Path(url).as_uri()
+    elif not urllib.parse.urlsplit(url).scheme:
+        # If it's a relative file path, convert it to an absolute path first, then to a proper URL
+        url = Path(os.path.abspath(url)).as_uri()
+
+    print(f"Starting the test on URL: {url}")
+
     # OpenAI key loading would be required here
     with open("openai_key.json", "r") as file:
         openai.api_key = json.load(file)["key"]
@@ -61,15 +74,23 @@ def main(url, delay, interactions, load_wait_time, test_type):
         input for input in inputs if not input.get_attribute("readonly")
     ]
 
+    print(f"Found {len(clickable_elements)} clickable elements.")
+
     # Start testing
     for i in range(interactions):
         if test_type == "monkey":
             element = random.choice(clickable_elements)
             if element.tag_name == "button":
+                print(
+                    f"Action {i+1}: Clicking button with outerHTML: '{element.get_attribute('outerHTML')}'."
+                )
                 element.click()
             elif element.tag_name == "input":
                 random_text = "".join(
                     random.choices(string.ascii_letters + string.digits, k=5)
+                )
+                print(
+                    f"Action {i+1}: Entering text into input field with outerHTML: '{element.get_attribute('outerHTML')}', text: '{random_text}'."
                 )
                 element.send_keys(random_text)
         else:
@@ -80,10 +101,12 @@ def main(url, delay, interactions, load_wait_time, test_type):
                 f"Please generate the next action to interact with this web page. "
                 f"Try to cover as many different features and edge cases as possible."
             )
+
             # Ask the GPT model for the next action
             response = openai.ChatCompletion.create(
                 model="gpt-4", messages=[{"role": "user", "content": prompt}]
             )
+
             action = response["choices"][0]["message"]["content"]
             exec(action)  # Execute the action
 
